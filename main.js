c8fb5567b44db78b0a959de4437c5fe689cde2db
FrankOrtegon <frank.ortegon01@gmail.com>
2021-05-25 19:42:02 -0500

    Se creó el metodo para mover el objeto ball, y se agrego la funcionalidad de pausar el juego

--- main.js
+++ main.js
@@ -6,11 +6,12 @@
     this.gameOver = false;
     this.bars = [];
     this.ball = null;
+    this.playing = false;
 }
 
 self.Board.prototype = {
     get elements(){
-        var elements = this.bars;
+        var elements = this.bars.map(function(bar) { return bar; });
         elements.push(this.ball);
         return elements;
     }
@@ -25,9 +26,17 @@ self.Board.prototype = {
         this.board = board;
         this.speed_y = 0;
         this.speed_x = 3;
+        this.derection = 1;
 
         board.ball = this;
         this.kind = "circle";
+    }
+    
+    self.Ball.prototype = {
+        move: function() {
+           this.x += (this.speed_x * this.derection);
+           this.y += (this.speed_y);
+        }
     }
 })();
 
@@ -78,8 +87,11 @@ self.Board.prototype = {
         },
 
         play: function() {
-            this.clean();
-            this.draw();
+            if (this.board.playing) {
+                this.clean();
+                this.draw();
+                this.board.ball.move();
+            }
         }
     }
 
@@ -108,24 +120,36 @@ var board_view = new BoardView(canvas,board);
 var ball = new Ball(350,100,10,board);
 
 document.addEventListener("keydown",function(ev) {
-    ev.preventDefault
+    
     if (ev.keyCode == 38) {
+        ev.preventDefault();
         bar.up(); //Flecha Arriba
     }
     else if(ev.keyCode == 40 ) {
+        ev.preventDefault();
         bar.down(); //Flecha Abajo
     }
     else if(ev.keyCode == 87) {
+        ev.preventDefault();
         bar2.up(); //W
     }
     else if (ev.keyCode == 83) {
+        ev.preventDefault();
         bar2.down(); //S
+    }
+    else if(ev.keyCode === 32) {
+        ev.preventDefault();
+        board.playing = !board.playing;
     }
 });
 
 window.requestAnimationFrame(controller);
+setTimeout(function() {
+    ball.derection = -1;
+},4000);
 
 function controller() {
+    
     board_view.play();
     window.requestAnimationFrame(controller);
 
